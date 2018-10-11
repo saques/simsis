0b4106d3ac5306f5ac318cb3616cfeb88f3ba461
saques <msaques@itba.edu.ar>
2018-10-10 23:15:13 -0300

    works but some explosions occur

--- src/main/java/tp5/Main.java
+++ src/main/java/tp5/Main.java
@@ -3,23 +3,48 @@ package tp5;
 import common.ParticleGenerators;
 import utils.PointDumper;
 
+import java.io.IOException;
 import java.util.Random;
 
 public class Main {
     static int seed = 2;
     static int M = 20;
-    static int N = 1000;
-    static float L = 5, W = 5, radius = 0.03f, mass = 0.01f;
-    static double MaxTime = 100, DeltaTime = 7E-5;
-    static double k = 1E5, gamma = 100, mu = 0.07;
-    static double D = 0.1;
+    static int N = 100;
+    static float L = 2, W = 2, radius = 0.03f, mass = 0.01f;
+    static double MaxTime = 5, DeltaTime = 7E-5;
+    static double k = 1E5, gamma = 100, mu = 0.1;
+    static double D = .15;
     static int dumpEach = 1000;
 
 
     public static void main(String [] args) throws Exception {
+        runBeeman();
+    }
+
+
+    private static void runGear() throws Exception {
         Random r = new Random(seed);
         DynamicGrid grid = new DynamicGrid(L, W, M, D);
         ParticleGenerators.generateGranularParticles(L, W, N, radius, mass, k, gamma, mu, r).forEach(grid::add);
+
+        //grid.add(new GranularParticle(1, 1,  0, 0, radius, mass, k, gamma, mu));
+        //grid.add(new GranularParticle(1, (1 + .5),  0, 0, radius, mass, k, gamma, mu));
+
+        PointDumper dumper = new PointDumper("./tp5/ovito/", PointDumper.FileMode.DYNAMIC, PointDumper.Dimensions._2D);
+
+        int frame = 0;
+        for (int i = 0; i < MaxTime/DeltaTime; i ++) {
+            boolean dump = i % dumpEach == 0;
+            grid.update(frame, DeltaTime, dumper, dump);
+            if(dump)
+                frame++;
+        }
+    }
+
+    private static void runBeeman() throws Exception{
+        Random r = new Random(seed);
+        DynamicGridBeeman grid = new DynamicGridBeeman(L, W, M, D);
+        ParticleGenerators.generateBeemanGranularParticles(L, W, N, radius, mass, k, gamma, mu, r).forEach(grid::add);
 
 //        grid.add(new GranularParticle(1, 1,  0, 0, radius, mass, k, gamma, mu));
 //        grid.add(new GranularParticle(1, (1 + .5),  0, 0, radius, mass, k, gamma, mu));
